1f780b7db0fc16e80a95f2f737592ae0a229e495
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-05-22 15:34:23 -0300

    Feat: Adicionando o valor total do pedido

--- app/(home)/product/[id]/orderConfirmation/buttonOrderConfirmation.tsx
+++ app/(home)/product/[id]/orderConfirmation/buttonOrderConfirmation.tsx
@@ -6,25 +6,34 @@ const ButtonOrderConfirmation = () => {
     const handleFinalizeOrder = () => {
         const cartItems = JSON.parse(localStorage.getItem("carrinho") || "[]");
         const selectedAddress = JSON.parse(localStorage.getItem("selectedAddress") || "{}");
-    
-        let message = "Meu pedido:\n";
+
+        let message = "*Meu pedido:*\n";
+        let total = 0;
         cartItems.forEach((item : any) => {
-          message += `${item.qtd}x ${item.name}\n`;
+          message += `- ${item.qtd}x ${item.name}\n`;
+          
+          if (typeof item.qtd === 'number' && typeof item.price === 'number') {
+            total += item.qtd * item.price; 
+          } else {
+            console.log('Invalid item quantity or price:', item.qtd, item.price);
+          }
         });
-    
+
+        message += `\n*Total: R$ ${total.toFixed(2)}*\n`; 
+
         if (selectedAddress) {
-          message += "\nEndereço de Entrega:\n";
+          message += "\n*Endereço de Entrega:*\n";
           message += `${selectedAddress.name}, ${selectedAddress.num}\n`;
           message += `${selectedAddress.bairro}\n`;
           if (selectedAddress.complemento) {
             message += `${selectedAddress.complemento}\n`;
           }
         }
-    
+
         const encodedMessage = encodeURIComponent(message);
-    
+
         const whatsappUrl = `https://wa.me/+558394045922?text=${encodedMessage}`;
-    
+
         window.location.href = whatsappUrl;
       };
 
